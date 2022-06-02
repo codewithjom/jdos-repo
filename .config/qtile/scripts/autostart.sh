aaf680b8ecc63f5f4efe12c8e7996cdec6aa5eec
Jom Dollesin <codewithjom@gmail.com>
2022-06-02 08:29:03 +0800

    JDOS.

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -3,3 +3,11 @@
 conky -c $HOME/.config/qtile/scripts/conkyrc &
 sxhkd -c ~/.config/qtile/scripts/sxhkdrc &
 nitrogen --set-scaled --restore &
+
+lxsession &
+picom --experimental-backends -b &
+nm-applet &
+volumeicon &
+xsetroot -cursor_name left_ptr &
+pamac-tray &
+blueberry-tray &
